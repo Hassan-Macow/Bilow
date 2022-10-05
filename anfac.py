db6f1f01a5adb625039ae6bc746eff5e95fbb820
Hassan Macow <hsnmac7@gmail.com>
2022-10-05 17:27:29 -0600

    Adeega Anfac

---
+++ anfac.py
@@ -0,0 +1,303 @@
+while True:
+    Xirmo = int(input("Fadlan Dooro:   \n 1. Adeega Anfac "))
+    if Xirmo == 1:
+        print("1. ANFAC Gudaha  ")
+        print("2. ANFAC Gudaha iyo Dibadda ")
+        print("3. ANFAC dibadda 32 dal ")
+        print("4. ANFAC SMS  ")
+        print("0. BACK  ")
+        while True:
+            Xirmo = int(input(" "))
+            if Xirmo == 1:
+                print("1. xirmo USD1 : ")
+                print("2. xirmo USD5 : ")
+                print("3. xirmo USD10 : ")
+                print("4. xirmo USD20 : ")
+                print("5. xirmo USD50 : ")
+                print("6.xirmo 90 sinti ")
+                print("7.xirmo 50 si0.back next99")
+                while True:
+                    Xirmo = int(input(" Fadlan Dooro Xirmo :"))
+                    if Xirmo == 1:
+                        print("waxaad heleysaa boqol daqiiqio wicitaanka gudaha.")
+                        print("Fadlan hubi isdiiwaangelinta: ")
+                        print("1.Maya")
+                        print("2.Haa")
+                        while True:
+                            Xirmo = int(input("dooro tiro :"))
+                            if Xirmo == 1:
+                                print("waad ku geeleysatay inaad iska joojiso howahaan")
+                            elif Xirmo == 2:
+                                print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                            else:
+                                print("macsalaama")
+                                break
+                            break
+                    elif Xirmo == 2:
+                         print("waxaad heleysaa 750 daqiiqio wicitaanka gudaha.")
+                         print("Fadlan hubi isdiiwaangelinta: ")
+                         print("1.Maya")
+                         print("2.Haa")
+                         while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+                    elif Xirmo == 3:
+                          print("waxaad heleysaa 1500 daqiiqio wicitaanka gudaha.")
+                          print("Fadlan hubi isdiiwaangelinta: ")
+                          print("1.Maya")
+                          print("2.Haa")
+                          while True:
+                              Xirmo = int(input("dooro tiro :"))
+                              if Xirmo == 1:
+                                  print("waad ku geeleysatay inaad iska joojiso howahaan")
+                              elif Xirmo == 2:
+                                  print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                              else:
+                                  print("macsalaama")
+                                  break
+                              break
+                    elif Xirmo == 4:
+                          print("waxaad heleysaa 3200 daqiiqio wicitaanka gudaha.")
+                          print("Fadlan hubi isdiiwaangelinta: ")
+                          print("1.Maya")
+                          print("2.Haa")
+                          while True:
+                              Xirmo = int(input("dooro tiro :"))
+                              if Xirmo == 1:
+                                  print("waad ku geeleysatay inaad iska joojiso howahaan")
+                              elif Xirmo == 2:
+                                  print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                              else:
+                                  print("macsalaama")
+                                  break
+
+                    elif Xirmo == 5:
+                         print("waxaad heleysaa 6400 daqiiqio wicitaanka gudaha.")
+                         print("Fadlan hubi isdiiwaangelinta: ")
+                         print("1.Maya")
+                         print("2.Haa")
+                         while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+                    elif Xirmo == 6:
+                        print("waxaad heleysaa 90 daqiiqio wicitaanka gudaha.")
+                        print("Fadlan hubi isdiiwaangelinta: ")
+                        print("1.Maya")
+                        print("2.Haa")
+                        while True:
+                            Xirmo = int(input("dooro tiro :"))
+                            if Xirmo == 1:
+                                print("waad ku geeleysatay inaad iska joojiso howahaan")
+                            elif Xirmo == 2:
+                                print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                            else:
+                                print("macsalaama")
+                                break
+                            break
+            elif Xirmo == 2 :
+                 print("1. xirmo USD5 : ")
+                 print("2. xirmo USD10 : ")
+                 print("3. xirmo USD20 : ")
+                 print("4. xirmo USD50 : ")
+                 print("0. kabax : ")
+                 while True:
+                     Xirmo = int(input(" Fadlan Dooro Xirmo :"))
+                     if Xirmo == 1 :
+                          print("waxad heleysaa:300daqiiqo wicitaan gudaha,200sms gudaha,6daqiiqo wicitaan dibadda ah,2sms dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                          while True:
+                            Xirmo = int(input("dooro tiro :"))
+                            if Xirmo == 1:
+                                print("waad ku geeleysatay inaad iska joojiso howahaan")
+                            elif Xirmo == 2:
+                                print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                            else:
+                                print("macsalaama")
+                                break
+                            break
+                     elif Xirmo == 2:
+                         print("waxad heleysaa:600daqiiqo wicitaan gudaha,200sms gudaha,18daqiiqo wicitaan dibadda ah,5sms dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                         while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+
+                             break
+                     elif Xirmo == 3:
+                         print("waxad heleysaa:1300daqiiqo wicitaan gudaha,200sms gudaha,36daqiiqo wicitaan dibadda ah,10sms dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                         while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+                     elif Xirmo == 4:
+                         print("waxad heleysaa:3500daqiiqo wicitaan gudaha,200sms gudaha,90daqiiqo wicitaan dibadda ah,10sms dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                         while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+
+            elif Xirmo == 3 :
+                print("1. xirmo USD5 : ")
+                print("2. Wadamada dibadda : ")
+                print("3. xirmo USD10 : ")
+                print("4. xirmo USD20 : ")
+                print("5. xirmo USD50 : ")
+                print("0. kabax : ")
+                while True:
+                    Xirmo = int(input(" Fadlan Dooro Xirmo :"))
+                    if Xirmo == 1 :
+                        print("waxad heleysaa:30daqiiqo dibadda ah .Fadlan iska hubi Liiaka wadamada iyo isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+
+                            Xirmo = int(input("dooro tiro :"))
+                            if Xirmo == 1:
+                                print("waad ku geeleysatay inaad iska joojiso howahaan")
+                            elif Xirmo == 2:
+                                print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                            else:
+                                print("macsalaama")
+                                break
+                            break
+                    elif Xirmo == 2:
+                            print("1.Wadamada Qeybta 1aad \n 2.Wadamada Qeybta 2aad \n 3.Wadamada Qeybta 3aad \n 4.Wadamada Qeybta 4aad \n 5.Wadamada Qeybta 5aad \n 0.Back Next:9")
+                            while True:
+
+                              Xirmo = int(input("dooro Qeyb :"))
+                              if Xirmo == 1:
+                                  print("Canada \n USA \n Inidia \n China \n Greece \n Ethopia \n UAE \n 0.Back")
+                              elif Xirmo == 2:
+                                  print("Malaysia \n Holland \n Sweden \n Norway \n Kenya \n Pakistan \n 0.Back")
+                              elif Xirmo == 3:
+                                  print("Turkey \n Saudi Arabia \n Switzerland \n Ireland \n Mexico \n 0.Back")
+                              elif Xirmo == 4:
+                                  print("Hungry \n Belgium \n France \n Spain \n Austria \n Morocco \n 0.Back")
+                              elif Xirmo == 5:
+                                  print("Romania \n Kuwait \n Bangladesh \n Bulgaria \n Denmark \n UK \n 0.Back")
+
+                              else:
+                                  print("macsalaama")
+                                  break
+                              break
+                    elif Xirmo == 3:
+                        print("waxad heleysaa:60 daqiiqo dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+                    elif Xirmo == 4:
+                        print("waxad heleysaa:120 daqiiqo dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+                    elif Xirmo == 5:
+                        print("waxad heleysaa:300 daqiiqo dibadda.Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+
+
+            elif Xirmo == 4 :
+                print("1. xirmo 50 cent gudaha : ")
+                print("2. xirmo USD1 gudaha : ")
+                print("3. xirmo USD1 dibadda : ")
+                print("0. kabax : ")
+                while True:
+                    Xirmo = int(input(" Fadlan Dooro Xirmo :"))
+                    if Xirmo == 1 :
+                        print("waxad heleysaa:80sms gudaha ah .Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+
+                            Xirmo = int(input("dooro tiro :"))
+                            if Xirmo == 1:
+                                print("waad ku geeleysatay inaad iska joojiso howahaan")
+                            elif Xirmo == 2:
+                                print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                            else:
+                                print("macsalaama")
+                                break
+                            break
+                    elif Xirmo == 2:
+                        print("waxad heleysaa:200sms gudaha ah .Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+                             Xirmo = int(input("dooro tiro :"))
+                             if Xirmo == 1:
+                                 print("waad ku geeleysatay inaad iska joojiso howahaan")
+                             elif Xirmo == 2:
+                                 print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                             else:
+                                 print("macsalaama")
+                                 break
+                             break
+                    elif Xirmo == 3:
+                        print("waxad heleysaa:20sms dibadda ah .Fadlan hubi isdiiwaangelintaada : \n 1.Maya \n 2.Haa")
+                        while True:
+                            Xirmo = int(input("dooro tiro :"))
+                            if Xirmo == 1:
+                                print("waad ku geeleysatay inaad iska joojiso howahaan")
+                            elif Xirmo == 2:
+                                print("waad ku geeleysatay inaa iska diiwangeliso xirmadaan")
+                            else:
+                                print("macsalaama")
+                                break
+                            break
+            elif Xirmo == 0:
+                Xirmo = int(input("Fadlan Dooro:   \n 1. Adeega Anfac "))
+                if Xirmo == 1:
+                    print("1. ANFAC Gudaha  ")
+                    print("2. ANFAC Gudaha iyo Dibadda ")
+                    print("3. ANFAC dibadda 32 dal ")
+                    print("4. ANFAC SMS  ")
+
+                    print("0. BACK  ")
+            else:
+                print("macsalaama")
